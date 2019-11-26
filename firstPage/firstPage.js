f308ee4c0ba1ed5d657f6cf27d11ae50e3926a05
Genevieve Koffman <genkoffman@gmail.com>
2019-11-26 12:01:44 +0200

    Update firstPage.js

--- firstPage/firstPage.js
+++ firstPage/firstPage.js
@@ -3,7 +3,7 @@ function createNewChat(host){
     id = Math.floor(Math.random() * 1000000);  
     id = `a${id}`;
     
-    DB.collection("Chats").doc(id).collection("Users").add({username: host}.then(doc=>{checkIDexists(id)}));
+    DB.collection("Chats").doc(id).collection("Users").add({username: host});
     DB.collection("Chats").doc(id).collection("Messages").add({messages: "none"})
     //DB.collection("Chats").doc(id).set({host: host});
     document.getElementById("generalLoginBox").style.display = "none";
@@ -33,30 +33,44 @@ function addUser(event){
 
     let username = document.getElementById('myName');
     let groupId = document.getElementById('groupID');
-     
-    console.log("checking: " + checkIDexists(groupId.value))
-    if(checkIDexists(groupId.value)){
-        document.getElementById("generalLoginBox").style.display = "none";
+
+    DB.collection("Chats").doc(groupId.value).get().then(docDB=> {
+        console.log(docDB.data())
+        if (docDB.exists) {
+            document.getElementById("generalLoginBox").style.display = "none";
         DB.collection('Chats').doc(id).collection('Users').add({
             username: username.value
         })
-    }
-}
-
-//collection document docDB .exists()
-
-function checkIDexists(groupID){
-    console.log(groupID)
-    DB.collection("Chats").doc(groupID).get().then(docDB=> {
-        console.log(docDB.data())
-        if (docDB.exists) {
-            console.log("true")
-            //return true;
         } else {
             console.log("false")
-            //return false;
+            return "false";
         }
     }).catch(function(error) {
         console.log("Error getting document:", error);
     });
+     
+    // if(checkIDexists(groupId.value) == "true"){
+    //     document.getElementById("generalLoginBox").style.display = "none";
+    //     DB.collection('Chats').doc(id).collection('Users').add({
+    //         username: username.value
+    //     })
+    // }
 }
+
+//collection document docDB .exists()
+
+// function checkIDexists(groupID){
+//     console.log(groupID)
+//     DB.collection("Chats").doc(groupID).get().then(docDB=> {
+//         console.log(docDB.data())
+//         if (docDB.exists) {
+//             console.log("true")
+//             return "true";
+//         } else {
+//             console.log("false")
+//             return "false";
+//         }
+//     }).catch(function(error) {
+//         console.log("Error getting document:", error);
+//     });
+// }
